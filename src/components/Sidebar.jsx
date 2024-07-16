5bd29c1f5f93652cc5846736dcb62720c36ba8d1
ars1mm <arsimajvazi420@gmail.com>
2024-07-16 02:37:45 +0200

    finishing SideBar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,8 +9,23 @@ const Sidebar = () => {
                 <img className='w-6' src={assets.home_icon} alt="" />
                 <p className='font-bold'>Home</p>
             </div>
+            <div className="flex items-center gap-3 pl-8 cursor-pointer">
+                <img className='w-6' src={assets.search_icon} alt="" />
+                <p className='font-bold'>Search</p>
+            </div>
         </div>
-      
+        <div className='bg-[#121212] h-[85%] rounded'>
+            <div className='p-4 flex items-center justify-between'>
+                <div className="flex items-center gap-3">
+                    <img className='w-8' src={assets.stack_icon} alt="" />
+                    <p className='semi-bold'>Your Library</p>
+                </div>
+                <div className='flex items-center gap-3'>
+                    <img className='w-5' src={assets.arrow_icon}alt="" />
+                    <img className='w-5' src={assets.plus_icon}alt="" />
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
