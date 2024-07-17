34b4b2fe40b868aeb93216184d8be2a42c609a69
ars1mm <arsimajvazi420@gmail.com>
2024-07-17 02:26:18 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from './components/Sidebar.jsx'
 import Player from './components/Player.jsx'
+import Display from './components/Display.jsx'
 
 const App = () => {
   return (
     <div className='bg-black h-screen'>
       <div className='h-[90%] flex '>
         <Sidebar />
+        <Display/>
       </div>
       <Player />
     </div>
---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './navbar'
+
+
+const DisplayHome = () => {
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default DisplayHome
